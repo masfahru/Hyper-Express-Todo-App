c4761931f38c461783c287683d3bc58774c02652
Imam Fahrur Rofi <imam@fahru.my.id>
2022-09-11 14:33:24 +0700

    Updating MySQL Engine

--- migrations/20220903094644_init_db.js
+++ migrations/20220903094644_init_db.js
@@ -4,34 +4,29 @@
  */
 exports.up = async function (knex) {
   return await Promise.all([
-    knex.raw(
-      `DROP TABLE IF EXISTS activities, todos`,
-    ),
+    knex.raw(`DROP TABLE IF EXISTS activities, todos`),
     knex.raw(
       `CREATE TABLE activities (
         id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-        email VARCHAR(250) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
-        title VARCHAR(100) NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+        email VARCHAR(250) NOT NULL DEFAULT '',
+        title VARCHAR(100) NULL DEFAULT '',
         created_at TIMESTAMP NULL DEFAULT current_timestamp(),
         updated_at TIMESTAMP NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-        PRIMARY KEY (id) USING BTREE,
-        INDEX email (email) USING HASH
+        PRIMARY KEY (id) USING BTREE
     )
-    COLLATE='latin1_swedish_ci'
-    ENGINE=MYISAM`,
+    ENGINE=MEMORY`,
     ),
     knex.raw(
       `CREATE TABLE todos (
   id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  title VARCHAR(100) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+  title VARCHAR(100) NULL DEFAULT NULL,
   activity_group_id INT(10) UNSIGNED NULL DEFAULT '0',
   is_active TINYINT(1) UNSIGNED NULL DEFAULT '1',
-  priority ENUM('very-high','high','normal','low','very-low') NULL DEFAULT 'very-high' COLLATE 'latin1_swedish_ci',
+  priority ENUM('very-high','high','normal','low','very-low') NULL DEFAULT 'very-high',
   PRIMARY KEY (id) USING BTREE,
   INDEX activity_group_id (activity_group_id) USING BTREE
 )
-COLLATE='latin1_swedish_ci'
-ENGINE=MyISAM`,
+ENGINE=MEMORY`,
     ),
   ]);
 };
